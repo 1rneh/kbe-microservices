fd20c39ff8df2c711c0c1b709c020050cc5fbb00
henri <henr1@mailbox.org>
2022-03-29 14:18:51 +0200

    fixed delivery info model : added Data annotation

--- storage-service/src/main/java/storage/DeliveryInfo.java
+++ storage-service/src/main/java/storage/DeliveryInfo.java
@@ -1,16 +1,20 @@
 package storage;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 @Document
 public class DeliveryInfo {
 
     @Id
     private String id;
 
+    @Indexed(unique = true)
     private String name;
 
     private Date deliveryTime;
